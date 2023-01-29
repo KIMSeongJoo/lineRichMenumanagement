7e7ba43f68156c5684f6d35110cf8644ee8313db
seongjookim <rrhh234@naver.com>
2023-01-29 15:57:40 +0900

    - axios 처리 비동기 처리화 - 리치메뉴 유저 링크 페이지 validate 처리 변경

--- components/UserRichmeu.vue
+++ components/UserRichmeu.vue
@@ -1,49 +1,68 @@
 <template>
-  <v-row>
-    <!-- user id field -->
-    <v-col cols="12">
-      <label class="text-h4">送信ユーザ</label>
-      <v-text-field
-        v-model="targetUser"
-        :counter="64"
-        :rules="rules"
-        label="LINE UID"
-      ></v-text-field>
-    </v-col>
-    <!-- rich menu id field -->
-    <v-col cols="12">
-      <label class="text-h4">リッチーメニューID</label>
-      <v-text-field
-        v-model="richmenuId"
-        :counter="64"
-        :rules="rules"
-        label="RICH MENU ID"
-      ></v-text-field>
-    </v-col>
-    <!-- send button area -->
-    <v-col cols="12">
-      <v-btn
-        elevation="2"
-        large
-        depressed
-        color="primary"
-        @click="setupRichmenu"
-        >送信</v-btn
-      >
-    </v-col>
-  </v-row>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-row>
+      <!-- user id field -->
+      <v-col cols="12">
+          <label class="text-h4">送信ユーザ</label>
+          <v-text-field
+            v-model="targetUser"
+            :counter="max"
+            :rules="riciMenuRules"
+            label="LINE UID"
+            required
+          ></v-text-field>
+      </v-col>
+      <!-- rich menu id field -->
+      <v-col cols="12">
+        <label class="text-h4">リッチーメニューID</label>
+        <v-text-field
+          v-model="richmenuId"
+          :counter="max"
+          :rules="lineUIDRules"
+          label="RICH MENU ID"
+          required
+        ></v-text-field>
+      </v-col>
+      <!-- send button area -->
+      <v-col cols="12">
+        <v-btn
+          :disabled="!valid"
+          elevation="2"
+          large
+          depressed
+          color="primary"
+          @click="validate"
+          >送信</v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script>
 import Swal from 'sweetalert2'
+import axios from 'axios'
 
 export default {
   name: 'UserRichmeu',
   data() {
     return {
-      rules: [v => v.length <= 64 || 'Max 1024 characters'],
-      targetUser: null,
-      richmenuId: null,
+      riciMenuRules: [
+        v => !!v || 'richmenu id は必須です',
+        v => (v && v.length <= 64) || 'richmenu idは最大64文字以内で入力してください。',
+      ],
+      lineUIDRules: [
+        v => !!v || 'line uidは必須です',
+        v => (v && v.length <= 64) || 'line uidは最大64文字以内で入力してください。',
+      ],
+      targetUser: '',
+      richmenuId: '',
+      valid: true,
+      max: 64,
     }
   },
   computed: {
@@ -52,33 +71,44 @@ export default {
     },
   },
   methods: {
-    setupRichmenu() {
+    validate () {
+      this.$refs.form.validate()
+      if (this.valid) {
+        const params = {
+          'apikey' : this.envInfo.apiKey,
+          'uid' : this.targetUser,
+          'ricimenuId' : this.richmenuId,
+        }
+        this.setupRichmenu(params)
+      }
+    },
+    setupRichmenu : async (params) => {
       const headers = {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + this.envInfo.apiKey,
+        Authorization: 'Bearer ' + params.apikey,
       }
 
       const url =
-        '/v2/bot/user/' + this.targetUser + '/richmenu/' + this.richmenuId
-      this.$axios
-        .post(url, '', { headers })
-        .then(() => {
-          Swal.fire({
-            icon: 'success',
-            title: '作成完了',
-            text: 'ユーザー設定完了',
-          })
-        })
-        .catch(({ e }) => {
-          if (e.response) {
+        '/v2/bot/user/' + params.uid + '/richmenu/' + params.ricimenuId
+
+      try {
+        await axios
+          .post(url, '', { headers })
+          .then(() => {
             Swal.fire({
-              icon: 'error',
-              title: 'リッチーメニューとユーザー情報のリンクに失敗しました。',
-              text: e,
+              icon: 'success',
+              title: '作成完了',
+              text: 'ユーザー設定完了',
             })
-          }
+          })
+      } catch (e) {
+        Swal.fire({
+          icon: 'error',
+          title: '作業失敗',
+          text: e.response.statusText,
         })
-    },
+      }
+    }
   },
 }
 </script>
